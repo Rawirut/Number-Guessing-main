e73992c03055877c58e24dd9646c4be846428725
GeatS_IX <rawirut.da.65@ubu.ac.th>
2025-07-24 16:22:27 +0700

    Update Gameplay.cs
    Add Emoji and Computer AI

--- Assets/Scripts/Gameplay.cs
+++ Assets/Scripts/Gameplay.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using System.Collections;
+using NUnit.Framework;
 using System.Collections.Generic;
 
 public class Gameplay : MonoBehaviour
@@ -48,12 +49,12 @@ public class Gameplay : MonoBehaviour
         int guess;
         if (!int.TryParse(input, out guess))
         {
-            gameState.text = "<sprite=10> Please enter a valid number";
+            gameState.text = "<sprite=15> Please enter a valid number";
             return;
         }
         if (guess < minNumber || guess > maxNumber)
         {
-            gameState.text = $"<sprite=10> Please enter a number between {minNumber} - {maxNumber}";
+            gameState.text = $"<sprite=15> Please enter a number between {minNumber} - {maxNumber}";
             return;
         }
         ProcessGuess(guess, true);
@@ -70,7 +71,7 @@ public class Gameplay : MonoBehaviour
         if (guess == targetNumber)
         {
             //Win
-            gameLog.text += $"<sprite=\"animals-nature\" index=9>{playerName} got it right!\n";
+            gameLog.text += $"<sprite=\"animals-nature\" index=9> {playerName} got it right!\n";
             EndGame();
         }
         else if (currentAttemps >= maxAttemps)
@@ -83,7 +84,7 @@ public class Gameplay : MonoBehaviour
         {
             //Wrong guess - give hint
             string hint = guess < targetNumber ? "Too Low" : "Too High";
-            gameLog.text += $"<sprite=\"animals-nature\" index=9>{hint}\n";
+            gameLog.text += $"<sprite=\"animals-nature\" index=16> {hint}\n";
 
             //Switch players
             isPlayerTurn = !isPlayerTurn;
@@ -95,10 +96,11 @@ public class Gameplay : MonoBehaviour
                 guessInputField.interactable = false;
                 submitButton.interactable = false;
                 StartCoroutine(ComputerTurn(guess < targetNumber));
+                //StartCoroutine(ComputerTurn(higher));
             }
             else
             {
-                guessInputField.interactable= true;
+                guessInputField.interactable = true;
                 submitButton.interactable = true;
                 guessInputField.Select();
                 guessInputField.ActivateInputField();
@@ -115,23 +117,24 @@ public class Gameplay : MonoBehaviour
             int lastGuess = computerGuesses[computerGuesses.Count - 1];
             if (targetIsHigher)
             {
+                // Too Low
                 computerMinGuess = lastGuess + 1;
+                //computerMaxGuess = lastGuess - 1;
             }
             else
             {
+                // Too High
                 computerMaxGuess = lastGuess - 1;
+                //computerMinGuess = lastGuess + 1;
             }
         }
 
-
-        //AI uses Binary Search strategy
-        int computerGuess =(computerMinGuess + computerMaxGuess) / 2;
+        // AI uses Binary Search strategy
+        Debug.Log($"{targetIsHigher} {computerMinGuess} {computerMaxGuess}");
+        int computerGuess = (computerMinGuess + computerMaxGuess) / 2;
 
         computerGuesses.Add(computerGuess);
 
-        ProcessGuess(computerGuess, false);
-
-        //int computerGuess = Random.Range(minNumber, maxNumber + 1);
         ProcessGuess(computerGuess, false);
     }
 
@@ -167,9 +170,7 @@ public class Gameplay : MonoBehaviour
         computerMinGuess = minNumber;
         computerMaxGuess = maxNumber;
         computerGuesses = new List<int>();
-
     }
-
 
     void Start()
     {
@@ -179,6 +180,6 @@ public class Gameplay : MonoBehaviour
 
     void Update()
     {
-        
+
     }
 }
